11213506094a8fba77ff63bbed6dba096af43935
AlexNDRmac <alexndr.mac@gmail.com>
2022-03-12 15:08:08 +0200

    Fix constants, fix tests

--- tests/test_common.py
+++ tests/test_common.py
@@ -3,22 +3,14 @@ import pytest as pytest
 from ripper.common import convert_size
 
 
-# @pytest.mark.parametrize('actual_ip, expected_result', [
-#     ('127.0.0.1', '127.*.*.*'),
-#     ('42.199.100.200', '42.*.*.*'),
-#     ('42', '42')
-# ])
-# def test_get_first_ip_part(actual_ip, expected_result):
-#     assert get_first_ip_part(actual_ip) == expected_result
-
-
-@pytest.mark.parametrize('actual, expected', [
-    (0, '0.00 B'),
-    (100, '100.00 B'),
-    (1024, '1.00 kB'),
-    (16384096, '15.63 MB'),
-    (32256798429, '30.04 GB'),
-    (620832256798429, '564.64 TB'),
-])
-def test_convert_size(actual, expected):
-    assert convert_size(actual) == expected
+class TestCommon:
+    @pytest.mark.parametrize('actual, expected', [
+        (0, '0.00 B'),
+        (100, '100.00 B'),
+        (1024, '1.00 kB'),
+        (16384096, '15.63 MB'),
+        (32256798429, '30.04 GB'),
+        (620832256798429, '564.64 TB'),
+    ])
+    def test_convert_size(self, actual, expected):
+        assert convert_size(actual) == expected
