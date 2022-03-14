1881e379c465b5b793dfda53469474d1f27858c6
PXEiYyMH8F <100959332+PXEiYyMH8F@users.noreply.github.com>
2022-03-14 19:15:11 +0000

    Add files via upload

--- DRipper.py
+++ DRipper.py
@@ -1,18 +1,1 @@
-import sys
-import signal
-from ripper.services import main
-
-
-if __name__ == '__main__':
-    try:
-        sys.exit(main())
-    except KeyboardInterrupt:  # The user hit Control-C
-        sys.stderr.write('\n\nReceived keyboard interrupt, terminating.\n\n')
-        sys.stderr.flush()
-        # Control-C is fatal error signal 2, for more see
-        # https://tldp.org/LDP/abs/html/exitcodes.html
-        sys.exit(128 + signal.SIGINT)
-    except RuntimeError as exc:
-        sys.stderr.write(f'\n{exc}\n\n')
-        sys.stderr.flush()
-        sys.exit(1)
+print('Deprecated. Please, call "dripper ..." instead of "python DRipper.py ..."')
