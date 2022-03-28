88812720a712576a59f49c17e89b97e5c900bb22
AlexNDRmac <alexndr.mac@gmail.com>
2022-03-28 14:39:56 +0300

    Fix setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 from ripper.constants import VERSION
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='dripper',
     version=VERSION,
     url='https://github.com/alexmon1989/russia_ddos',
+    python_requires=">=3.9",
     packages=find_packages(exclude=['tests']),
-    install_requires=['rich==12.0.0', 'pysocks==1.7.1'],
+    install_requires=required,
     package_data={
         'ripper': [
             'headers.txt',
