e61f483cdf409a6fb45ea0ca6b2d17640ca99273
AlexNDRmac <alexndr.mac@gmail.com>
2022-03-28 19:06:04 +0300

    Update setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 from ripper.constants import VERSION
 
-with open('requirements.txt') as f:
+with open('requirements.txt', 'r', encoding='utf-8') as f:
     required = f.read().splitlines()
+
+with open('README.md', 'r', encoding='utf-8') as md:
+    readme = md.read()
 
 setup(
     name='dripper',
     version=VERSION,
+    long_description=readme,
+    long_description_content_type='text/markdown',
     url='https://github.com/alexmon1989/russia_ddos',
     python_requires=">=3.9",
     packages=find_packages(exclude=['tests']),
