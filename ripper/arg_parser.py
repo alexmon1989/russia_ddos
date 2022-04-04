40a94aa4ff1c6eadf585d1a8b4a94862f9ff4430
Oleksandr Knyga <oleksandrknyga@gmail.com>
2022-04-04 20:34:29 +0300

    underscore changed for hyphen in args options

--- ripper/arg_parser.py
+++ ripper/arg_parser.py
@@ -20,30 +20,30 @@ def parser_add_options(parser: OptionParser) -> None:
                       action='append',
                       help='Attack target in {scheme}://{hostname}[:{port}][{path}] format. Multiple targets allowed')
     parser.add_option('-m', '--method',
-                      dest='attack_method', type='str',
+                      dest='attack-method', type='str',
                       help=f'Attack method: {", ".join(attack_method_labels)}')
-    parser.add_option('-e', '--http_method',
+    parser.add_option('-e', '--http-method',
                       dest='http_method', type='str', default=ARGS_DEFAULT_HTTP_ATTACK_METHOD,
                       help=f'HTTP method. Default: {ARGS_DEFAULT_HTTP_ATTACK_METHOD}')
     parser.add_option('-t', '--threads',
                       dest='threads_count', type='int', default=ARGS_DEFAULT_THREADS_COUNT,
                       help=f'Total threads count. Default: {ARGS_DEFAULT_THREADS_COUNT}')
-    parser.add_option('-r', '--random_len',
+    parser.add_option('-r', '--random-len',
                       dest='random_packet_len', type='int', default=ARGS_DEFAULT_RND_PACKET_LEN,
                       help=f'Send random packets with random length. Default: {ARGS_DEFAULT_RND_PACKET_LEN}')
-    parser.add_option('-l', '--max_random_packet_len',
+    parser.add_option('-l', '--max-random_packet-len',
                       dest='max_random_packet_len', type='int', default=ARGS_DEFAULT_MAX_RND_PACKET_LEN,
                       help=f'Max random packets length. Default: {ARGS_DEFAULT_MAX_RND_PACKET_LEN} for udp/tcp')
-    parser.add_option('-y', '--proxy_list',
+    parser.add_option('-y', '--proxy-list',
                       dest='proxy_list',
                       help='File (fs or http/https) with proxies in ip:port:username:password line format. Proxies will be ignored in udp attack!')
-    parser.add_option('-k', '--proxy_type',
+    parser.add_option('-k', '--proxy-type',
                       dest='proxy_type', type='str', default=ARGS_DEFAULT_PROXY_TYPE,
                       help=f'Type of proxy to work with. Supported types: socks5, socks4, http. Default: {ARGS_DEFAULT_PROXY_TYPE}')
-    parser.add_option('-c', '--health_check',
+    parser.add_option('-c', '--health-check',
                       dest='health_check', type='int', default=ARGS_DEFAULT_HEALTH_CHECK,
                       help=f'Controls health check availability. Turn on: 1, turn off: 0. Default: {ARGS_DEFAULT_HEALTH_CHECK}')
-    parser.add_option('-o', '--socket_timeout',
+    parser.add_option('-o', '--socket-timeout',
                       # default value is not set here to keep dynamic logic during initialization
                       dest='socket_timeout', type='int', default=ARGS_DEFAULT_SOCK_TIMEOUT,
                       help=f'Timeout for socket connection is seconds. Default (seconds): {ARGS_DEFAULT_SOCK_TIMEOUT} without proxy, {2*ARGS_DEFAULT_SOCK_TIMEOUT} with proxy')
