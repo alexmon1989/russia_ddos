0431b894988949247046591225648dab6d11d6e8
AlexNDRmac <alexndr.mac@gmail.com>
2022-04-09 09:46:01 +0300

    Fix test with version checker

--- tests/test_github_updates_checker.py
+++ tests/test_github_updates_checker.py
@@ -1,7 +1,6 @@
 import pytest
 import time
 
-from _version import __version__
 from ripper.github_updates_checker import GithubUpdatesChecker, Version
 
 
@@ -11,24 +10,24 @@ class DescribeGithubUpdatesChecker:
         tag_names = guc.fetch_tag_names()
         assert len(tag_names) > 0
         assert isinstance(tag_names[0], str)
-    
+
     def it_can_read_versions(self):
         guc = GithubUpdatesChecker()
         versions = guc.fetch_versions()
         assert len(versions) > 0
-        assert Version(__version__) <= versions[-1]
-    
+        assert Version('1.0.0') <= versions[-1]
+
     def it_can_read_latest_version(self):
         guc = GithubUpdatesChecker()
         latest_version = guc.fetch_lastest_version()
-        assert Version(__version__) <= latest_version
+        assert Version('1.0.0') <= latest_version
 
     def it_can_get_str_version(self):
         assert Version('2.3.1').version == '2.3.1'
-    
+
     def it_can_read_latest_version_on_background(self):
         guc = GithubUpdatesChecker()
         guc.demon_update_latest_version()
         while guc.latest_version is None:
             time.sleep(1)
-        assert Version(__version__) <= guc.latest_version
+        assert Version('1.0.0') <= guc.latest_version
