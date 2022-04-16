e3b10529a0f7161a10547b00e0922d5626f5ec16
Oleksandr Knyga <oleksandrknyga@gmail.com>
2022-04-16 22:47:35 +0300

    1K packets per second

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,31 +1,32 @@
 import pytest as pytest
+import time
 
 from ripper.common import convert_size, detect_cloudflare, generate_fixed_size_random_bytes
 
 
 class DescribeCommonMethods:
-    # @pytest.mark.parametrize('actual, expected', [
-    #     (0, '0.00 B'),
-    #     (100, '100.00 B'),
-    #     (1024, '1.00 kB'),
-    #     (16384096, '15.63 MB'),
-    #     (32256798429, '30.04 GB'),
-    #     (620832256798429, '564.64 TB'),
-    #     (620832256798429256, '551.41 PB'),
-    # ])
-    # def it_has_convert_size(self, actual, expected):
-    #     assert convert_size(actual) == expected
+    @pytest.mark.parametrize('actual, expected', [
+        (0, '0.00 B'),
+        (100, '100.00 B'),
+        (1024, '1.00 kB'),
+        (16384096, '15.63 MB'),
+        (32256798429, '30.04 GB'),
+        (620832256798429, '564.64 TB'),
+        (620832256798429256, '551.41 PB'),
+    ])
+    def it_has_convert_size(self, actual, expected):
+        assert convert_size(actual) == expected
 
-    # @pytest.mark.parametrize('actual, units, expected', [
-    #     (1024, 'B/s', '1.00 kB/s'),
-    #     (1024, 'Bps', '1.00 kBps'),
-    #     (16384096, 'B/s', '15.63 MB/s')
-    # ])
-    # def it_has_convert_size_with_units(self, actual, units, expected):
-    #     assert convert_size(actual, units) == expected
+    @pytest.mark.parametrize('actual, units, expected', [
+        (1024, 'B/s', '1.00 kB/s'),
+        (1024, 'Bps', '1.00 kBps'),
+        (16384096, 'B/s', '15.63 MB/s')
+    ])
+    def it_has_convert_size_with_units(self, actual, units, expected):
+        assert convert_size(actual, units) == expected
 
-    # def it_can_check_cloudflare_protection(self):
-    #     assert detect_cloudflare('https://www.thesfmarathon.com') is True
+    def it_can_check_cloudflare_protection(self):
+        assert detect_cloudflare('https://www.thesfmarathon.com') is True
 
     def it_can_generate_fixed_size_random_sequence(self):
         b1 = generate_fixed_size_random_bytes(10)
@@ -34,3 +35,9 @@ class DescribeCommonMethods:
         assert len(b2) == 10
         assert b1 == b1
         assert b1 != b2
+        start = time.time()
+        for _ in range(1000):
+            generate_fixed_size_random_bytes(65536)
+        duration = time.time() - start
+        # 1K packets per second
+        assert duration < 1
